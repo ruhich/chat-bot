4b82443fde82e428cd30b2587bcd72903f783302
ruhich <ruslanbazrov43@gmail.com>
2024-07-21 04:07:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,39 +5,37 @@ function sendMessage() {
 
 	if (userMessage === '') return
 
-	
 	const userMessageElement = document.createElement('div')
 	userMessageElement.className = 'message user-message'
 	userMessageElement.textContent = userMessage
 	chatBox.appendChild(userMessageElement)
 
-	
 	userInput.value = ''
 
-	
 	chatBox.scrollTop = chatBox.scrollHeight
 
-	
 	setTimeout(() => {
 		const botMessageElement = document.createElement('div')
 		botMessageElement.className = 'message bot-message'
 		botMessageElement.textContent = getBotResponse(userMessage)
 		chatBox.appendChild(botMessageElement)
 
-		
 		chatBox.scrollTop = chatBox.scrollHeight
 	}, 1000)
 }
 
 function getBotResponse(message) {
-	
 	const responses = {
 		привет: 'Здравствуйте! Как я могу помочь?',
 		'как дела?': 'У меня всё отлично, спасибо! А у вас?',
 		пока: 'До свидания! Хорошего дня!',
-    'дашь мне печеньку': 'Спасибо, отдаю ее тебе' , 
-    'хочешь погулять?': 'прости, но я не могу гулять с тобой, я всего лишь бот(', 
-    'паритет легенда?' : 'Да, он лучший айтишник до 15 лет, я если его увижу афтограф возьму!'
+		'дашь мне печеньку': 'Спасибо, отдаю ее тебе',
+		'хочешь погулять?':
+			'прости, но я не могу гулять с тобой, я всего лишь бот(',
+		'паритет легенда?':
+			'Да, он лучший айтишник до 15 лет, я если его увижу автограф возьму!',
+		'давай поговорим' : 'Давай, о чем хочешь поболтать?' ,
+		'как тебе погода?' : 'Погода прекрасная' ,
 	}
 
 	return responses[message.toLowerCase()] || 'Извините, я не понимаю вас.'
